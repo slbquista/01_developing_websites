5e766fd795f31ed166a0add6dd5699913fe05175
Finn Turnbull <slbquista@gmail.com>
2017-11-14 17:23:54 +0000

    Planning and design

---
+++ ante_mortem_2/Design/antemortem_2.1/js/hamburger.js
@@ -0,0 +1,17 @@
+$( ".cross" ).hide();
+
+$( ".menu" ).hide();
+
+$( ".hamburger" ).click(function() {
+	$( ".menu" ).slideToggle(300, function() {
+		$( ".hamburger" ).hide();
+		$( ".cross" ).show();
+	});
+});
+
+$( ".cross" ).click(function() {
+	$( ".menu" ).slideToggle(300, function() {
+		$( ".cross" ).hide();
+		$( ".hamburger" ).show();
+	});
+});
